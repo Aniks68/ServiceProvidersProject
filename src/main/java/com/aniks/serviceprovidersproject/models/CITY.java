2cfcd042136b1eeed7f8b00a01a757f9d04cc13a
Aniks68 <ikechukwu.anene@decagon.dev>
2022-05-18 13:25:07 +0100

    added CITY enum and fields for Restaurant class

---
+++ src/main/java/com/aniks/serviceprovidersproject/models/CITY.java
@@ -0,0 +1,5 @@
+package com.aniks.serviceprovidersproject.models;
+
+public enum CITY {
+    ABUJA, LAGOS, IBADAN, UYO, PORT_HARCOURT, ENUGU, ASABA, KANO, UMUAHIA, ONITSHA, ABA, OWERRI;
+}
--- src/main/java/com/aniks/serviceprovidersproject/models/Restaurant.java
+++ src/main/java/com/aniks/serviceprovidersproject/models/Restaurant.java
@@ -1,4 +1,33 @@
 package com.aniks.serviceprovidersproject.models;
 
+import com.sun.istack.NotNull;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false)
+    private String name;
+
+    @NotNull
+    @Column(nullable = false)
+    private String emailAddress;
+
+    @NotNull
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @NotNull
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private CITY city;
+
 }
