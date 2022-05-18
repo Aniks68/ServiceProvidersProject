a29fbab2d6a80ac1e3c3d5b91ba82512e5b2f538
Aniks68 <ikechukwu.anene@decagon.dev>
2022-05-18 13:03:29 +0100

    initialising project with its dependencies

---
+++ src/main/java/com/aniks/serviceprovidersproject/ServiceProvidersProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aniks.serviceprovidersproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceProvidersProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceProvidersProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aniks/serviceprovidersproject/ServiceProvidersProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aniks.serviceprovidersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProvidersProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
